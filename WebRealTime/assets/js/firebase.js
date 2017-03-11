4341f4f9eb739de91d9cc859acf3592fd9817c17
Brian Guerrero <guerrero.a.brian@gmail.com>
2017-03-11 04:17:28 -0800

    Firebase Google LogIn Fully Functional. Tested on Firefox with a Localhost. Will be merging soon with frontend branch

--- WebRealTime/assets/js/firebase.js
+++ WebRealTime/assets/js/firebase.js
@@ -1,30 +1,42 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCk_P9M-wREfr100p9OMafbiVYripukhl8",
-  authDomain: "cash-login.firebaseapp.com",
-  databaseURL: "https://cash-login.firebaseio.com",
-  storageBucket: "cash-login.appspot.com",
-  messagingSenderId: "288160664366"
-};
-firebase.initializeApp(config);
+var app = document.querySelector('form.login-form');
+console.log(app);
 
-(function() {
-      var app = document.querySelector('#app');
-      /*app.signIn = function() {
-        var email = app.email;
-        var password = app.password;
-        if (!email || !password) {
-          return console.log('email and password required');
-        }
-        // Sign in user
-        firebase.auth().signInWithEmailAndPassword(email, password)
-          .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log('signIn error', error);
-            // ...
-          });
-      };*/
+function google_login_in(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    //provider.addScope('https://www.googleapis.com/auth/plus.login');
+    //provider.addScope('profile');
+    //provider.addScope('email');
+    //provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+    }).catch(function(error) {
+        var errorCode     = error.code;
+        var errorMessage  = error.message;
+        var email         = error.email;
+        var credential    = error.credential;
+    });
+}
 
-  })();
+//Logout
+function log_out() {
+    firebase.auth().signOut();
+    console.log("logged out");
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        console.log("User is signed In");
+        user.providerData.forEach(function (profile) {
+            console.log("Sign-in provider: "+profile.providerId);
+            console.log("  Provider-specific UID: "+profile.uid);
+            console.log("  Name: "+profile.displayName);
+            console.log("  Email: "+profile.email);
+            console.log("  Photo URL: "+profile.photoURL);
+        });
+    } else {
+        console.log("user signed out");
+        // No user is signed in.
+    }
+});
