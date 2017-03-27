69fb5802e04c57de363ca7a7e3c439fa6e6fccdf
Brian Guerrero <guerrero.a.brian@gmail.com>
2017-03-27 16:59:46 -0700

    AJAX and JSON working example added to CASH.html

--- WebRealTime/assets/js/script.js
+++ WebRealTime/assets/js/script.js
@@ -7,4 +7,33 @@ $(function() {
       
     }
     //$('#container').hide();
+    /*var obj = $('section form p:nth-child(3)');
+    obj.append( "<span> - 2nd!</span>" );*/
+    //console.log(obj);
+
+    $('#get-data').click(function () {
+        //alert( "Handler for .click() called." );
+        var showData = $('#show-data');
+
+        $.getJSON('../data/example.json', function (data) {
+            console.log(data);
+
+            /*var items = data.items.map(function (item) {
+                return item.key + ': ' + item.value;
+            });
+
+            showData.empty();
+
+            if (items.length) {
+                var content = '<li>' + items.join('</li><li>') + '</li>';
+                var list = $('<ul />').html(content);
+                showData.append(list);
+            }*/
+            var disp_data = data.glossary.title;
+            console.log(disp_data);
+            var obj = $('section form p:nth-child(3)').append("<span>" + disp_data + "</span>");
+        });
+
+        showData.text('Loading the JSON file.');
+    });
 });
