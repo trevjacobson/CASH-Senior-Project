589b81b85f0aba1bbaffcfd4310396e9124738ce
steakfiresBrianG <guerrero.a.brian@gmail.com>
2017-04-08 23:05:49 -0700

    added hide() and show() for component-cash. Next is loading the profiles from SQL tables.

--- WebRealTime/assets/js/script.js
+++ WebRealTime/assets/js/script.js
@@ -6,6 +6,42 @@ $(function() {
     function render(url) {
       
     }
+
+    /*$("a").click(function( event ) {
+       event.preventDefault();
+        $.get("profiles.html", function(data) {
+            $('div.component-cash').html(data);
+            console.log(data);
+        });
+    });*/
+
+    $("a").click(function(event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        console.log(href);
+        $('div.component-cash').show();
+        $('div.component-profile').hide(1000, function() {
+            $(this).hide();
+        });
+    });
+
+    $('#change-div').click(function () {
+        //$('div.component-cash').replaceWith('<h2>New heading</h2>');
+        var pathArray = window.location.pathname.split( '/' );
+        $('div.component-profile').replaceWith('<h2>New heading</h2>');
+        $('div.component-cash').hide(1000, function() {
+            $(this).hide();
+        });
+        console.log(pathArray);
+        /*$.get("profiles.html", function(data) {
+            $('div.component-profile').html(data);
+            $('div.component-cash').hide(1000, function() {
+                $(this).hide();
+            });
+            //$('div.component-cash').html(data);
+            console.log(data);
+        });*/
+    });
     //$('#container').hide();
     /*var obj = $('section form p:nth-child(3)');
     obj.append( "<span> - 2nd!</span>" );*/
