92c59db9a4d47046812472e20b1c0e9e93c9cf1d
Trevor Jacobson <trevjacobson@gmail.com>
2017-04-19 17:45:41 -0700

    Added doorSensor class

--- Door_Switch.py
+++ Door_Switch.py
@@ -1,4 +1,19 @@
 import RPi.GPIO as GPIO
+
+#read only class to detect the state of the door switch
+class doorSensor(object):
+
+    def __init__(self):
+        self.door = 1
+
+    def readDoorSensor(self):
+        if GPIO.input(22):
+            self.door = 0;
+            return 0;
+        else:
+            self.door = 1;
+            return 1;
+
 GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
 GPIO.setup(22, GPIO.INPUT, GPIO.PUD_UP) # set up pin ?? (one of the above listed pins) as an input with a pull-up resistor
 if GPIO.input(22):
